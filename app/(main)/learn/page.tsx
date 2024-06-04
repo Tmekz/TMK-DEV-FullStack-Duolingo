2ca09cae93b76444bb554a4400a75ae0abf743e6
Tarig Mekhazni <tmk-dev@hotmail.com>
2024-06-04 16:08:04 +0200

    preparing the right sidebar to receive dynamics data : lifes left etc ..

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,11 +1,19 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { HeaderLearnPage } from "./headerLearnPage";
+import { UserProgress } from "@/components/user-progress";
 
 const LearnPage = () => {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6   ">
-      <StickyWrapper>My sticky sidebar</StickyWrapper>
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        ></UserProgress>
+      </StickyWrapper>
       <FeedWrapper>
         <HeaderLearnPage title="Spanish" />
       </FeedWrapper>
