3d0e7c2bd8136ca1b9ae349f4419778d3ba9c27c
Tarig Mekhazni <tmk-dev@hotmail.com>
2024-06-05 17:11:09 +0200

    list of cards on courses page

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -6,7 +6,7 @@ const CoursesPage = async () => {
 
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto ">
-      <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>{" "}
+      <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
       <List courses={coursesData} activeCourseId={1} />
     </div>
   );
